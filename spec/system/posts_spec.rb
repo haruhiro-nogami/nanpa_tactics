4b6983e50a374cbf3c22c7d3d97a48a7114a6661
j.h.nogami <j.h.nogami@gmail.com>
2019-11-13 00:14:09 +0900

    Rspec

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,139 +1,137 @@
 require "rails_helper"
 
 describe "投稿管理機能", type: :system do
-  # let ユーザーAの作成準備
-  # let ユーザーBの作成準備
-  # let! ユーザーAの投稿を必ず作成
+  let(:user_a) { FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com") }# let ユーザーAの作成準備
+  let(:user_b) { FactoryBot.create(:user, name: "ユーザーB", email: "b@example.com") }# let ユーザーBの作成準備
+  let!(:post_a) { FactoryBot.create(:post, title: "テスト投稿A", user: user_a) }# let! ユーザーAの投稿を必ず作成
 
   before do
-    #ユーザーAの作成を実行 let
-    #ユーザーAの投稿を作成
-    # 共通化：誰かでログインする
+    visit login_path
+    fill_in "メールアドレス", with: login_user.email
+    fill_in "パスワード", with: login_user.password
+    click_button "ログインする"# 共通化：誰かでログインする
   end
 
   shared_examples_for "ユーザーAが作成した投稿が表示される" do
-    # 作成した投稿のタイトルが表示されていることを確認
+    it { expect(page).to have_content "テスト投稿"}# 作成した投稿のタイトルが表示されていることを確認
   end
 
   describe "一覧表示機能" do
-
-
     context "ユーザーAがログインしている時" do
-      # ユーザーAを作成してログイン let
-
+      let(:login_user) {user_a}# ユーザーAを作成してログイン let
       it_behaves_like "ユーザーAが作成した投稿が表示される"
-
     end
 
     context "ユーザーBがログインしている時" do
-        # ユーザーBを作成してログインlet
-
+      let(:login_user) {user_b}  # ユーザーBを作成してログインlet
       it "ユーザーAが作成した投稿が表示されない" do
-        # 表示されていないことを確認
+        expect(page).to have_no_content "テスト投稿"# 表示されていないことを確認
       end
-
     end
-
   end
 
   describe "詳細表示機能" do
     context "ユーザーAがログインしている時" do
-      # ユーザーAを作成してログイン
+      let(:login_user) {user_a}# ユーザーAを作成してログイン let
       before do
-        # 投稿を作成してアクセスvisit post_path(ポストA)
+        visit post_path(post_a)
       end
-
       it_behaves_like "ユーザーAが作成した投稿が表示される"
-
     end
-
   end
 
   describe "新規作成機能" do
-    # ユーザーAでログイン
+    let(:login_user) {user_a}# ユーザーAを作成してログイン let
 
     before do
       visit new_post_path
-      fill_in "title", with: post_name
-      click_button "Create"
+      fill_in "post_title", with: post_title
+      click_button "Create Post"
     end
 
     context "新規作成でタイトルを入力" do
-      # post_nameにタイトルを入れる
+      let(:post_title) { "新規作成テスト" }# post_nameにタイトルを入れる
       it "正常に登録" do
-        # 登録されたことを確認
+        expect(page).to have_selector ".alert-success",  text: "投稿に成功しました"
+        expect(page).to have_content "新規作成テスト"# 登録されたことを確認
       end
     end
 
     context "新規作成でタイトルが未入力" do
-      # post_nameに何も入れない
+      let(:post_title) { "" }# post_nameに何も入れない
       it "エラーになる" do
-        # エラー文が表示されている
+        within "#error_explanation" do
+          expect(page).to have_content "blank"# エラー文が表示されている
+        end
       end
     end
   end
 
   describe "投稿編集機能" do
-    # ユーザーAでログイン
-
+    let(:login_user) {user_a}# ユーザーAを作成してログイン let
     before do
-      visit edit_post_path
-      fill_in "title", with: "編集"
-      fill_in "description", with: post_description
-      click_button "Update"
+      visit post_path(post_a) #edit_post_pathだとidをどう指定していいのかわからず、苦肉の策
+      click_link "編集"
+      fill_in "post_title", with: "編集機能テスト"
+      fill_in "post_description", with: "編集しました"
+      click_button "Update Post"
     end
 
     context "詳細に一文を追加" do
-      post_descriptionに一文を入れる
+      before do
+        visit post_path(post_a)
+      end
       it "正常に更新" do
-        # 編集されたことを確認　成功しましたの一文認証とか
+        expect(page).to have_content "編集機能テスト"# 編集されたことを確認　成功しましたの一文認証とか
+        expect(page).to have_content "編集しました"
+
       end
     end
   end
-
-  # describe "投稿削除機能" do　必要ないか？
-  #   # ユーザーAでログイン
+  #
+  # describe "投稿削除機能" do
+  #   let(:login_user) {user_a}# ユーザーAを作成してログイン let
   #   before do
-  #     visit destroy_post_path
-  #     fill_in "description", with: post_description
-  #     click_button "削除"
+  #     click_link "テスト投稿A"
+  #     click_link "削除"
+  #     click_button "OK" ここがうまくいかない
   #   end
   #
   #   it "正常に削除" do
-  #
+  #     expect(page).to have_no_content "テスト投稿A"
   #   end
   #
   # end
-
-  describe "投稿検索機能" do
-    # ユーザーAでログイン
-
-    before do
-      # search_post_pathに移動
-      fill_in "タイトル検索" , with: search_name
-      click_button "Search"
-    end
-    context "「サンプル」で検索した場合" do
-      # search_nameにサンプルと入れる
-      it "タイトルに「サンプル」を含まない投稿が表示されない" do
-
-      end
-    end
-    context "詳細検索で複数の条件を入れた場合" do
-      # search_nameを入力
-      # 他の検索要素を入力
-      it "指定した条件に合わない投稿が表示されない" do
-
-      end
-    end
-    context "該当する投稿が一件もない場合" do
-      # search_nameを入力
-      # ありえない検索要素を入力      
-      it "「該当する投稿が見つかりません」と表示されている" do
-
-      end
-    end
-
-  end
-
+#
+#   describe "投稿検索機能" do
+#     # ユーザーAでログイン
+#
+#     before do
+#       # search_post_pathに移動
+#       fill_in "タイトル検索" , with: search_name
+#       click_button "Search"
+#     end
+#     context "「サンプル」で検索した場合" do
+#       # search_nameにサンプルと入れる
+#       it "タイトルに「サンプル」を含まない投稿が表示されない" do
+#
+#       end
+#     end
+#     context "詳細検索で複数の条件を入れた場合" do
+#       # search_nameを入力
+#       # 他の検索要素を入力
+#       it "指定した条件に合わない投稿が表示されない" do
+#
+#       end
+#     end
+#     context "該当する投稿が一件もない場合" do
+#       # search_nameを入力
+#       # ありえない検索要素を入力
+#       it "「該当する投稿が見つかりません」と表示されている" do
+#
+#       end
+#     end
+#
+#   end
+#
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,60 +3,62 @@ require "rails_helper"
 
 describe "ユーザー管理機能", type: :system do
 
+
   describe "新規登録機能" do
     before do
-      # 登録ページへ移動
+      visit new_admin_user_path
+      fill_in "名前", with: "ユーザーA"
       fill_in "メールアドレス" , with: email_name
+      fill_in "パスワード", with: "password"
+      fill_in "パスワード（確認）", with: "password"
+      click_button "登録する"
     end
     context "メールアドレスが入力されていない場合" do
-
-      # パスワードを入力
+      let(:email_name) {""}
       it "登録ページに戻りエラーを表示する" do
-
+          expect(page).to have_content "blank"
       end
     end
     context "データベースに同じメールアドレスが登録されている場合" do
-
-      email_nameをセット
-      fill_in "パスワード"
-      登録ボタンを押す
-      ログアウト
-      登録ページへ移動
-      fill_in "メールアドレス", with: 同じアドレス
-      fill_in "パスワード"
-      登録ボタンを押す
-
-
+      let(:email_name) {"a@example.com"}
+      before do
+        visit logout_path
+        visit new_admin_user_path
+        fill_in "メールアドレス" , with: "a@example.com"
+        fill_in "パスワード", with: "password"
+        fill_in "パスワード（確認）", with: "password"
+        click_button "登録する"
+      end
       it "エラー文を表示する" do
-        # 「そのメールアドレスは別のアカウントで使用されています」と表示される
+        expect(page).to have_content "Email has already been taken"# 「そのメールアドレスは別のアカウントで使用されています」と表示される
       end
     end
 
-    context "パスワードが未入力の場合" do
-      # メールアドレスを入力
-      it "登録ページに戻りエラーを表示する" do
-        # 登録ページへ戻り、「パスワードを入力してください」と表示される
-      end
-    end
-
-    context "パスワードとパスワードの確認がマッチしていない場合" do
-      # メールアドレスを入力
-      it "登録ページに戻りエラーを表示する" do
-        # 登録ページへ戻り、「パスワード確認が間違っています」と表示される
-      end
-    end
-
+#     context "パスワードが未入力の場合" do
+#       # メールアドレスを入力
+#       it "登録ページに戻りエラーを表示する" do
+#         # 登録ページへ戻り、「パスワードを入力してください」と表示される
+#       end
+#     end
+#
+#     context "パスワードとパスワードの確認がマッチしていない場合" do
+#       # メールアドレスを入力
+#       it "登録ページに戻りエラーを表示する" do
+#         # 登録ページへ戻り、「パスワード確認が間違っています」と表示される
+#       end
+#     end
+#
+#   end
+#
+#
+# # ここから下、必要か？　手動でいいかも
+# #   describe "編集機能" do
+# #
+# #   end
+# #
+# #   describe "退会機能" do
+# #
   end
-
-
-# ここから下、必要か？　手動でいいかも
-#   describe "編集機能" do
-#
-#   end
-#
-#   describe "退会機能" do
-#
-#   end
 
 
 end
